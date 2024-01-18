e9e527bef628f04bc32eec101b0ea6577f7ebc86
max-nz <115492742+max-nz@users.noreply.github.com>
2024-01-18 17:07:35 +1300

    tidy up ais coverage repo

--- queries/ais_coverage/measured_ais_reception.sql
+++ queries/ais_coverage/measured_ais_reception.sql
@@ -10,6 +10,7 @@ CREATE TEMP FUNCTION max_lat() AS ({max_lat});
 CREATE TEMP FUNCTION min_lon() AS ({min_lon});
 CREATE TEMP FUNCTION max_lon() AS ({max_lon});
 
+
 --
 WITH
 --
@@ -29,6 +30,7 @@ sat_ssvid as (
   SELECT
     ssvid,
     satellite_pos as sat_positions,
+    terrestrial_pos as ter_positions,
     lat,
     lon,
     EXTRACT(HOUR FROM timestamp) as hour,
@@ -62,6 +64,7 @@ sat_ssvid_final AS (
   SELECT
       ssvid,
       SUM(sat_positions) as sat_positions,
+      SUM(ter_positions) as ter_positions,
       AVG(lat) as lat,
       AVG(lon) as lon,
       hour,
@@ -69,6 +72,8 @@ sat_ssvid_final AS (
       AVG(interpolated_speed_knots) as interpolated_speed_knots,
       SUM(IF(type = 'A', sat_positions, 0)) as A_messages,
       SUM(IF(type = 'B', sat_positions, 0)) as B_messages,
+      SUM(IF(type = 'A', ter_positions, 0)) as A_ter_messages,
+      SUM(IF(type = 'B', ter_positions, 0)) as B_ter_messages,
       date
   FROM sat_ssvid
   GROUP BY ssvid, hour, hour_midpoint, date
@@ -83,9 +88,12 @@ by_half_day AS (
     MIN(interpolated_speed_knots) min_interpolated_speed_knots,
     MAX(interpolated_speed_knots) max_interpolated_speed_knots,
     SUM(sat_positions)/COUNT(*) sat_pos_per_hour,
+    SUM(ter_positions)/COUNT(*) ter_pos_per_hour,
     FLOOR(hour/12) day_half,
     SUM(A_messages) A_messages,
     SUM(B_messages) B_messages,
+    SUM(A_ter_messages) A_ter_messages,
+    SUM(B_ter_messages) B_ter_messages,
     date
   FROM sat_ssvid_final
   GROUP BY ssvid, date, day_half
@@ -99,7 +107,8 @@ reception_quality AS (
       FLOOR(a.lon * 10) / 10 as lon_bin,
       IF(by_half_day.A_messages > 0, "A", "B") as class,
       COUNT(*) as hours,
-      AVG(sat_pos_per_hour) * 24 as sat_pos_per_day
+      AVG(sat_pos_per_hour) * 24 as sat_pos_per_day,
+      AVG(ter_pos_per_hour) * 24 as ter_pos_per_day
     FROM sat_ssvid_final a
     JOIN by_half_day
       ON a.ssvid = by_half_day.ssvid
